2c169697e4e9cbc039ed198a096457655ba01cbc
Gabriel Santos <gabriel.santos@macron.com.br>
2024-12-17 14:08:39 -0300

    primeiro commite

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gabriel & Bruno Music</title>
+    <link rel= "stylesheet" type = "text/css" href="./assets/css/style.css">
+</head>
+<header class="header">
+<a class="logo">Gabriel & Bruno Music</a> 
+   <Nav class="nav">
+    <button class="btn-mobile">menu</button>
+    <ul class="menu">
+        <li><a href="./paginas/artista.php">Artista</a></li>
+        <li><a href="./paginas/musica.php">Musica</a></li>
+        <li><a href="./paginas/album.php">Album</a></li>
+        <li><a href="./paginas/genero.php">Gênero</a></li>    
+    </ul>
+    </Nav>
+</header>
+<body>
+</body>
+</html>
---
+++ site_music-pdo.php
@@ -0,0 +1,27 @@
+<?php
+try {
+	$pdo = new PDO ("mysql:host=localhost;dbname=site_music","root", "");
+ 
+
+} 
+catch (PDOException $e) {
+   echo "erro com banco de dados; ".$e->getMessage();
+}
+catch (Exception $e)
+{
+ echo "erro generico;" .$e->getMessage();;
+}
+
+//$res = $pdo-> prepare("INSERT INTO artista (nome) VALUES 
+	//(:n )");
+
+//$res->bindValue(":n", "Gian");
+//$res->execute();
+
+
+$teste = $pdo-> query ("SELECT *FROM artista order by id_artista desc limit 3");
+$teste2 = $teste-> fetchAll(PDO::FETCH_ASSOC);
+echo'<pre>';
+print_r($teste2);
+?>
+
