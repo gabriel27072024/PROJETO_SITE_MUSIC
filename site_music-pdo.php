05455f560d2212bada66e43dd03479d1ce440def
Gabriel Santos <silvasantosgabrie789@gmail.com>
2024-12-20 11:45:42 -0300

    Feito Select no banco de dados

--- site_music-pdo.php
+++ site_music-pdo.php
@@ -1,6 +1,6 @@
 <?php
-$nome ="Gustavo Lima";
-$id = "35";
+$nome ="Jhenifer";
+$id = "36";
 try {
 	$pdo = new PDO ("mysql:host=localhost;dbname=site_music","root", "");
  
@@ -17,7 +17,7 @@ catch (Exception $e)
 $res = $pdo-> prepare("INSERT INTO artista (nome) VALUES 
 	(:n )");
 
-$res->bindValue(":n", "Abrão");
+$res->bindValue(":n", "Larissa");
 $res->execute();
 
 
@@ -28,3 +28,12 @@ $res = $pdo->prepare ("UPDATE artista SET nome = :n WHERE id = :id");
 $res->bindValue(":n", $nome);
 $res->bindValue(":id", $id);
 $res->execute();
+
+
+$res = $pdo->prepare("SELECT*FROM artista where id = :id");
+$res->bindValue(":id" , $id);
+$res->execute();
+$resultado = $res->fetchAll(PDO::FETCH_ASSOC);
+echo"<pre>";
+print_r($resultado);
+echo"</pre>";
